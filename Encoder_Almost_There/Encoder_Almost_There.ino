8dd565e9ea2935cea446070ea135ea95692a33b2
Jemma Tenenbaum <46406704+JemmaTbaum@users.noreply.github.com>
2019-11-20 17:44:34 -0800

    Add files via upload
    Working version.

--- Encoder_Almost_There/Encoder_Almost_There.ino
+++ Encoder_Almost_There/Encoder_Almost_There.ino
@@ -3,22 +3,37 @@
   #define INB 4
 
   #define outputA 3
-  #define outputB 5
-  
-  int mot_speed = 255;
-  int rotDirection = 0;
-  int pwm_speed =20;
+  #define outputB 2
 
-  int counter = 0;
+  int INA_State;
+  int INB_State;
+  int16_t i;
 
-  int shake;
-  int bake;
+  volatile int counter = 0;
+
+  int shake_prevA;
+  int bake_prevB;
   int aState;
-  int revsCount = 0;
-  int ana_Speed = map (pwm_speed,0,100,0,255); //Ask software team, how will the motor know to change spin direction? 
+
+  // Define controller variables
+  int16_t set_pt;  // Desired angular position
+  int16_t pos;  // Measured encoder angular position
+  int16_t error = 0;  // Difference between desired angular position and actual angular position
+  int16_t e_dif = 0;  // Time derivative of error, used for differential control
+  int16_t old_error = 0; // Calculated error from the last controller loop
+  int16_t e_sum = 0; // Error summation
+  double Kp = 0.5;
+  double Kd = 0.3;
+  double Ki = 0.0025;
+  int16_t Pout = 0;  // Proportional control output
+  int16_t Dout = 0;  // Derivative control output
+  int16_t Iout = 0;  // Integral control output
+  int16_t act = 0;  // actuation signal to motor
+  int8_t dt = 1; // in milliseconds
+  int16_t max_act = 100; // Set this based on testing
+  int16_t min_act = 0; // Set this based on testing
 
 void setup() {
-
   
   //pinMode(PWM, OUTPUT);
   pinMode(INA, OUTPUT);
@@ -28,62 +43,98 @@ void setup() {
   pinMode(outputB, INPUT);
 
   Serial.begin(9600);
-  
-  // Set initial rotation direction
-  // set_pt = (encoder reader function) // manually hold the link vertically at balanced point to obtain desired setpoint
-  // Serial.println(set_pt);            // display desired encoder position
-  digitalWrite(INA, HIGH);
-  digitalWrite(INB, LOW);
- 
-  shake = digitalRead(outputA);
- // bake = digitalRead(outputB);
-    analogWrite(PWM, 20);
+
+  attachInterrupt(digitalPinToInterrupt(outputA), A_Rise, RISING);  
+  attachInterrupt(digitalPinToInterrupt(outputB), B_Rise, RISING);
+
+  // int pwm_speed = map (mot_speed,0,100,0,255); //Ask software team, how will the motor know to change spin direction? 
+
+//  shake = digitalRead(outputA);
+// // bake = digitalRead(outputB);
+//    analogWrite(PWM, 20);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
- 
 
+  i = 0;
+  set_pt = 800; // manually hold the link vertically at balanced point to obtain desired setpoint
+  
+  //Serial.println(set_pt);            // display desired encoder position
 
-if (revsCount == 5) {
-  //Serial.println(revsCount);
-  analogWrite(PWM, 0);
-  Serial.println("Motor 1 rev");
-  Serial.println(revsCount);
-}
-//Serial.println(revsCount);
-
-    // write code that prompts user for desired values of Kp, Ki, Kd, and a new setpoint (in Deg)
-    // need a function to relate degrees to changes in encoder values
+  for (i=0; i<5000; i++) // Run the controller for 5 seconds
+    {
+      pos = counter; // Determine current motor angular position 
+      
+      error = set_pt - pos;  // obtain positional error by comparing to desired angular position
+      
+      e_dif = (error - old_error)/dt;  // Set up Differential parameters: How the error is changing per run 
+      old_error = error;  // Store error for next loop
     
-  // PID_control(5, 0, 0, set_pt); //Kp, Ki, Kd values are temp. subject to user change
+      e_sum = e_sum + error*dt;  // Set up integral parameters: The sum of the error throughout the program
+      
+      Pout = Kp*error;  // Calculate proportional control
+    
+      Dout = Kd*e_dif;  // Calculate differential control
+    
+      Iout = Ki*e_sum;  // Calculate integral control
+    
+      act = Pout+Dout+Iout; // Calculate actuation magnitude (PWM)
 
-
- /*  aState = digitalRead(outputA); // Reads the "current" state of the outputA
-   // If the previous and the current state of the outputA are different, that means a Pulse has occured
-   if (aState != shake){     
-     // If the outputB state is different to the outputA state, that means the encoder is rotating clockwise
-     if (digitalRead(outputB) != aState) { 
-       counter ++;
-     } else {
-       counter --;
-     }
-     Serial.print("Position: ");
-     Serial.println(counter);
-   } 
-
-   shake = aState;
-
-*/
-   attachInterrupt(digitalPinToInterrupt(outputA), blink, RISING);  
+      // Set rotation direction and negate act if necessary
+      rot_direction();
+      
+      // Saturate the actuation signal
+      if (act>max_act){
+        act = max_act;
+      }
+      else if(act<min_act){
+        act = min_act;
+      }
+      
+      analogWrite(PWM, act); // Assuming this is the output control signal to motor PWM
+      Serial.println(counter);
+      delay(dt); //sets function to run once, per ms
+    }
+    Serial.println(counter);
+    
 }
 
-void blink() {
-  counter++;
-  //Serial.println(counter);
+// Truth and table for encoder signals
+// A rising and B low = CW motion
+// A rising and B high = CCW motion
+// B rising and A low = CCW motion
+// B rising and A high = CW motion
 
-     if(counter == 399){
-      revsCount++;
-      counter = 0;   
-   }
+void A_Rise() { // Interrupt Service Routine for when encoder A reading transitions from low to high (Rising edge)
+  if (digitalRead(outputB)){  // If encoder B is high (and A is rising), the motor is moving Counter Clock Wise
+    counter --; // CCW = decrement
+  }
+  else{
+    counter ++; // CW = increment
+  }
+}
+
+void B_Rise() { // Interrupt Service Routine for when encoder A reading transitions from low to high (Rising edge)
+  if (digitalRead(outputA)){  // If encoder B is high (and A is rising), the motor is moving Counter Clock Wise
+    counter ++; // CW = increment
+  }
+  else{
+    counter --; // CCW = decrement
+  }
+}
+
+void rot_direction(){
+  
+  if (act<=0){
+    INA_State = HIGH;
+    INB_State = LOW;
+    act *= -1;
+  }
+  else if(act>0){
+    INA_State = LOW;
+    INB_State = HIGH;
+  }
+  
+  digitalWrite(INA, INA_State);
+  digitalWrite(INB, INB_State);
 }
